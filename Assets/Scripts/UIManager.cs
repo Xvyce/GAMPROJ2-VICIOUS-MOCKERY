7aa9f5fb678213b4866767ff058e78f57bec6673
kitendaya <kitendaya1@gmail.com>
2021-06-30 15:11:19 +0800

    Added UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : Singleton<UIManager>
+{
+
+    public GameObject WinScreen;
+
+    public bool isWin;
+
+    // UIManager.Instance.WinScreen
+    // Start is called before the first frame update
+    void Start()
+    {
+        isWin = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWin == true)
+        {
+            WinScreen.SetActive(true);
+        }
+
+        if (isWin == false)
+        {
+            WinScreen.SetActive(false);
+        }
+    }
+}
