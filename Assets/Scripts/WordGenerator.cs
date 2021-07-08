373f5cd7f3c49cfcbbb458962691d990501d149f
andrei-excel <andrei.excel2@gmail.com>
2021-07-08 19:49:51 +0800

    Update WordGenerator.cs

--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -8,7 +8,7 @@ public class WordGenerator : MonoBehaviour
     private static string[] tutorialEasy = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
                                             "t", "u", "v", "w", "x", "y", "z"   };
 
-    private static string[] tutorialNormal = {"one", "day", "you",  };
+    private static string[] tutorialNormal = {"one", "day", "you"  };
 
 
 
