6a74ef20e71fe6f7c9a2aa73a2046b9199e54300
andrei-excel <andrei.excel2@gmail.com>
2021-07-23 00:30:03 +0800

    Caster Projectile Test
    Made slime prefab
    
    Slime projectiles spawn a few seconds, need to add conditions for when they're only allowed to spawn

--- Assets/Scripts/Enemy/CasterSkill.cs
+++ Assets/Scripts/Enemy/CasterSkill.cs
@@ -8,6 +8,10 @@ public class CasterSkill : MonoBehaviour
     private Enemy _enemy;
     [SerializeField] Transform spawnPoint;
 
+    [SerializeField] private GameObject slimePrefab;
+    [SerializeField] private int slimesToSpawn;
+    [SerializeField] private float timeBetweenSpawns;
+
     private void Awake()
     {
         _enemyManager = GameObject.FindWithTag("EnemyManager").GetComponent<EnemyManager>();
@@ -21,22 +25,25 @@ public class CasterSkill : MonoBehaviour
 
     private IEnumerator Stop() // stops the movement of casters
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(4f);
         _enemy.isWalking = false;
         StartCoroutine(Cast());
     }
 
     private IEnumerator Cast()
     {
-        //add animation for casting spell
+        //enable casting animation
         yield return new WaitForSeconds(5f);
+        //disable casting animation
 
         // After finished casting, censors words then resumes walking
-        foreach(Enemy enemy in _enemyManager.enemyList)
-        {
-            //censor text of  current enemies that are spawned in the scene
-            enemy.StartCensor();
-        }
+        //foreach(Enemy enemy in _enemyManager.enemyList)
+        //{
+        //    //censor text of  current enemies that are spawned in the scene
+        //    enemy.StartCensor();
+        //}
+
+        StartCoroutine(SpawnSlime());
 
         /*
          * WaitForSeconds(seconds)
@@ -44,5 +51,16 @@ public class CasterSkill : MonoBehaviour
          * If(typed = true) do nothing
          * If(typed = false) start spawning projectiles
          */
+    }
+
+    IEnumerator SpawnSlime()
+    {
+        for (int i = 0; i < slimesToSpawn; i++)
+        {
+            Instantiate(slimePrefab, spawnPoint.position, Quaternion.Euler(30, 0, 0));
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
+
+        yield break;
     }
 }
