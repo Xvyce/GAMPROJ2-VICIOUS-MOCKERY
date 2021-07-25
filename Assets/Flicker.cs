24ddcf08514a387f83a5c4a8c810eda94a0021a1
Hushshsh <spframos123@gmail.com>
2021-07-25 20:46:01 +0800

    visual updates (main menu)

--- Assets/Flicker.cs
+++ Assets/Flicker.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Flicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Light candleLight;
+    public float timeDelay;
+
+    private void Start()
     {
+        candleLight = GetComponent<Light>();
+    }
+    private void Update()
+    {
+        //if()
+        //{
+        //    StartCoroutine(FlickeringLight());
+        //}
+        candleLight.intensity = ;
+    }
+
+    IEnumerator FlickeringLight()
+    {
+       
+       
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+      
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
