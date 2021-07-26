2bdd55247b88085052377c667d54e14cec08bc73
Xvyce <77040737+Xvyce@users.noreply.github.com>
2021-07-27 02:36:50 +0800

    Level1 cutscene

---
+++ Assets/Scripts/videoScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class videoScript : MonoBehaviour
+{
+
+    public VideoPlayer VideoPlayer; // Drag & Drop the GameObject holding the VideoPlayer component
+    public string SceneName;
+    void Start()
+    {
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
